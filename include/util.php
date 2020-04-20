808bc4bef73526adb97ad74d22888b027b742590
Glitch (tart-cemetery) <none>
2020-04-20 19:38:38 +0000

    🏥🏉 Checkpoint ./include/util.php:86128/508

--- include/util.php
+++ include/util.php
@@ -1,5 +1,15 @@
 <?php
-  
+function checked(&$something, $compare) {
+  if (isset($something) && (is_array($something) && in_array($compare, $something) || $something == $compare)) {
+    return "checked";
+  }
+  return "";
+}
+
+function value(&$something, $default = "") {
+  return isset($something) ? $something : $default;
+}
+
 function safeParam($arr, $index, $default="") {
   if ($arr && isset($arr[$index])) {
     return $arr[$index];
